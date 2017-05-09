e53e98f60d03d15171cd61e6d2ccc911b7330830
Joaquin Bravo Contreras <jackbravo@gmail.com>
2017-05-09 10:49:50 -0500

    Hacer el query en ArticleRestResource

--- modules/custom/flisol/src/Plugin/rest/resource/ArticleRestResource.php
+++ modules/custom/flisol/src/Plugin/rest/resource/ArticleRestResource.php
@@ -2,12 +2,15 @@
 
 namespace Drupal\flisol\Plugin\rest\resource;
 
+use Drupal\Core\Database\Connection;
 use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\StreamWrapper\PublicStream;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 /**
  * Provides a resource to get view modes by entity and bundle.
@@ -30,6 +33,20 @@ class ArticleRestResource extends ResourceBase {
   protected $currentUser;
 
   /**
+   * The database connection
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * The file system stream wrapper
+   *
+   * @var string
+   */
+  protected $fileDir;
+
+  /**
    * Constructs a new ArticleRestResource object.
    *
    * @param array $configuration
@@ -44,6 +61,10 @@ class ArticleRestResource extends ResourceBase {
    *   A logger instance.
    * @param \Drupal\Core\Session\AccountProxyInterface $current_user
    *   A current user instance.
+   * @param \Drupal\Core\Database\Connection $database
+   *   A connection to the drupal database.
+   * @param \Drupal\Core\StreamWrapper\PublicStream $stream
+   *   A wrapper for the public file system.
    */
   public function __construct(
     array $configuration,
@@ -51,10 +72,14 @@ class ArticleRestResource extends ResourceBase {
     $plugin_definition,
     array $serializer_formats,
     LoggerInterface $logger,
-    AccountProxyInterface $current_user) {
+    AccountProxyInterface $current_user,
+    Connection $database,
+    PublicStream $stream) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
 
     $this->currentUser = $current_user;
+    $this->database = $database;
+    $this->fileDir = $stream->getDirectoryPath();
   }
 
   /**
@@ -67,7 +92,9 @@ class ArticleRestResource extends ResourceBase {
       $plugin_definition,
       $container->getParameter('serializer.formats'),
       $container->get('logger.factory')->get('flisol'),
-      $container->get('current_user')
+      $container->get('current_user'),
+      $container->get('database'),
+      $container->get('stream_wrapper.public')
     );
   }
 
@@ -87,7 +114,28 @@ class ArticleRestResource extends ResourceBase {
       throw new AccessDeniedHttpException();
     }
 
-    return new ResourceResponse("Implement REST State GET!");
+    try {
+      $query = $this->database->select('node_field_data', 'nfd');
+      $query->condition('nfd.type', 'article');
+
+      $query->join('node__field_image', 'n_fi', 'n_fi.entity_id = nfd.nid');
+      $query->join('file_managed', 'f', 'f.fid = n_fi.field_image_target_id');
+
+      $query->addField('nfd', 'title');
+      $query->addExpression("REPLACE(f.uri, 'public:/', :base_path)", 'image', [':base_path' => $this->fileDir]);
+
+      // $string = $query->execute()->getQueryString();
+
+      $articles = $query->execute()->fetchAll(\PDO::FETCH_ASSOC);
+
+      $response = new ResourceResponse(['articles' => $articles]);
+      $response->setMaxAge(strtotime('1 day', 0));
+
+      return $response;
+    }
+    catch (\Exception $e) {
+      throw new BadRequestHttpException($this->t('Could not find articles'), $e);
+    }
   }
 
 }
